8f29f27abaf4151360c0877992e9edb40923459d
Sober9662 <duanzi0100>
2018-04-01 19:46:13 +0800

    chapter3-1-1：构建一个较为复杂的RESTful API以及单元测试

---
+++ chapter3-1-1/src/main/java/com/springboot/chapter311/webApi/UserController.java
@@ -0,0 +1,14 @@
+package com.springboot.chapter311.webApi;
+
+import com.springboot.chapter311.domain.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserController {
+
+}
